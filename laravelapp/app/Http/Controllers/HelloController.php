edf36c17e99229347adde1886c7f51cde782c31b
Hiromitsu Matsuura <urax@wd6.so-net.ne.jp>
2018-11-14 07:11:23 +0900

    adding multiple args

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -4,30 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HelloController extends Controller
-{
-    public function index($id='noname', $pass='unknown') {
-        return <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOF
 <style>
 body {font-size:16pt; color:#999; }
 h1 { font-size:100pt; text-align:right; color:#eee;
-    margin:-40px 0px -50px 0px;
+    margin:-40px 0px -50px 0px; }
 </style>
-</head>
-<body>
-    <h1>Index</h1>
-    <p>これは、Helloコントローラのindexアクションです。</p>
-    <ul>
-        <li>ID: {$id}</li>
-        <li>PASS: {$pass}</li>
-    </ul>
-</body>
-</html>
 EOF;
+$body = '</head><body>';
+$end = '</body></html>';
 
+function tag($tag, $txt) {
+    return "<{$tag}>" . $txt . "</{$tag}>";
+}
+
+class HelloController extends Controller
+{
+    public function index() {
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title', 'Hello/Index') . $style . $body 
+            . tag('h1','Index') . tag('p', 'this is Index page')
+            . '<a href="hello/other">go to 0ther page</a>'
+            . $end;
+        return $html;
     }
-    //
+    public function other() {
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title','Hello.Other') . $style . $body
+            . tag('h1','other') . tag('p','this is Other page') . $end;
+        return $html;
+    }
 }
