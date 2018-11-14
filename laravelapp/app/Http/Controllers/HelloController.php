9ec33cf5f0290aa7615bcd9afa5b98d69f96b874
Hiromitsu Matsuura <urax@wd6.so-net.ne.jp>
2018-11-15 01:23:31 +0900

    Adding request and response code

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -3,41 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<EOF
-<style>
-body {font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee;
-    margin:-40px 0px -50px 0px; }
-</style>
-EOF;
-$body = '</head><body>';
-$end = '</body></html>';
-
-function tag($tag, $txt) {
-    return "<{$tag}>" . $txt . "</{$tag}>";
-}
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function __invoke() {
-        return <<<EOF
+    public function index(Request $request, Response $response) {
+$html = <<<EOF
 <html>
 <head>
-<title>Hello</title>
+<title>Hello/Index</title>
 <style>
 body {font-size:16pt; color:#999; }
-h1 { font-size:30pt; text-align:right; color:#eee;
-    margin:-15px 0px 0px 0px; }
+h1 { font-size:120pt; text-align:right; color:#fafafa;
+    margin:-50px 0px -120px 0px; }
 </style>
 </head>
 <body>
-    <h1>Single Action</h1>
-    <p>これは、シングルアクションコントローラのアクションです。</p>
+    <h1>Hello</h1>
+    <h3>Request</h3>
+    <pre>{$request}</pre>
+    <h3>Response</h3>
+    <pre>{$response}</pre>
 </body>
 </html>
 EOF;
+        $response->setContent($html);
+        return $response;
     }
 }
