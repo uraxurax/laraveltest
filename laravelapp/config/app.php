823f6ba32e492c8ca21839cdc02e58333e3ccde5
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-23 02:21:36 +0900

    list 3-36

--- laravelapp/config/app.php
+++ laravelapp/config/app.php
@@ -172,7 +172,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
+        App\Providers\HelloServiceProvider::class,
     ],
 
     /*
