524331e5d9f6d6eae7c688613b431d4e46d6a0a6
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-22 11:21:27 +0900

    list3-15}

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -7,10 +7,20 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index($id='zero') 
+    public function index() 
     {
-        $data = ['msg'=>'これはコントローラから渡されたメッセージです。',
-    'id'=>$id];
+        $data = [
+            'msg'=>'お名前を入力してください。',
+        ];
         return view('hello.index', $data);
+    }
+
+    public function post(Request $request)
+    {
+        $msg = $request->msg;
+        $date = [
+            'msg'=>'こんにちは、' . $msg . 'さん！',
+        ];
+        return view('hello.index', $date);
     }
 }
