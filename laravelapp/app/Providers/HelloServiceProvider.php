1ae4342f966e87c44333148274a87359ac3ca566
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-23 02:44:59 +0900

    list 3-40

--- laravelapp/app/Providers/HelloServiceProvider.php
+++ laravelapp/app/Providers/HelloServiceProvider.php
@@ -15,9 +15,7 @@ class HelloServiceProvider extends ServiceProvider
     public function boot()
     {
         View::composer(
-            'hello.index', function($view) {
-                $view->with('view_message', 'composer message!');
-            }
+            'hello.index', 'App\Http\Composers\HelloComposer'
         );
         //
     }
