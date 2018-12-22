d17ad6bd0eea8c0dde35c6fe18832bc5b6ba8d33
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-23 02:43:56 +0900

    list 3-39

--- laravelapp/app/Http/Composers/HelloComposer.php
+++ laravelapp/app/Http/Composers/HelloComposer.php
@@ -1,4 +1,15 @@
 <?php
+
 namespace App\Http\Composers;
 
-use Illuminate\
+use Illuminate\View\View;
+
+class HelloComposer
+{
+
+    public function compose(View $view)
+    {
+        $view->with('view_message', 'this view is "' . $view->getName() . '"!!');
+
+    }
+}
