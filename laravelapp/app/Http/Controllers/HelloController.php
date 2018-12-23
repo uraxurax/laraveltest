b4e683cd7b6e8be34cc41018a073966fe5bb9858
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-24 08:57:13 +0900

    list 4-33

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -11,10 +11,23 @@ class HelloController extends Controller
 {
     public function index(Request $request) 
     {
-        return view('hello.index', ['msg'=>'フォームを入力ください。' ]);
+        if ($request->hasCookie('msg'))
+        {
+            $msg = 'Cookie: ' . $request->cookie('msg');
+        } else {
+            $msg = 'クッキーはありません';
+        }
+        return view('hello.index', ['msg'=>$msg]);
     }
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
-        return view('hello.index', ['msg'=>'正しく入力されました！']);
+        $validate_rule = [
+            'msg' => 'required',
+        ];
+        $this->validate($request, $validate_rule);
+        $msg = $request->msg;
+        $response = new Response(view('hello.index', ['msg'=>'[' . $msg . ']をクッキーに保存しました。']));
+        $response->cookie('msg', $msg, 100);
+        return $response;
     }
 }
