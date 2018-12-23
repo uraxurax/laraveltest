fd5c46e6076d008f4870c82f6d0e23a1499118ed
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-23 22:29:45 +0900

    list 4-23

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -12,8 +13,18 @@ class HelloController extends Controller
     {
         return view('hello.index', ['msg'=>'フォームを入力:']);
     }
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ]);
+        if ($validator->fails()) {
+            return redirect('/hello')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
