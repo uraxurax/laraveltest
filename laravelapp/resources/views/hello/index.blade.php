abf57cafbe1f560f329c12b22612cd27a7ce023f
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-23 19:35:04 +0900

    list 4-14

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -8,9 +8,16 @@
 @endsection
 
 @section('content')
-    <p>ここが本文のコンテンツです</p>
-    <p>これは,<middleware>google.com</middleware>へのリンクです。</p>    
-    <p>これは,<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
+    <p>{{$msg}}</p>
+    <table>
+        <form action="/hello" method="post">
+            {{ csrf_field() }}
+            <tr><th>name: </th><td><input type="text" name="name"></td></tr>
+            <tr><th>mail: </th><td><input type="text" name="mail"></td></tr>
+            <tr><th>age: </th><td><input type="text" name="age"></td></tr>
+            <tr><th></th><td><input type="submit" value="send"></td></tr>
+        </form>
+    </table>
 @endsection
     
 @section('foobar')
