83a8ee773bc67200b28d70552a88192abc8be605
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-23 21:02:33 +0900

    list 4-18

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -9,20 +9,23 @@
 
 @section('content')
     <p>{{$msg}}</p>
-    @if (count($errors) > 0) 
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+    @if (count($errors) > 0)
+    <p>入力に問題があります。再入力して下さい。</p>
     @endif
     <table>
         <form action="/hello" method="post">
             {{ csrf_field() }}
+            @if ($errors->has('name'))
+            <tr><th>ERROR</th><td>{{$errors->first('name')}}</td><tr>
+            @endif
             <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+            @if ($errors->has('mail'))
+            <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td><tr>
+            @endif
             <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+            @if ($errors->has('age'))
+            <tr><th>ERROR</th><td>{{$errors->first('age')}}</td><tr>
+            @endif
             <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
             <tr><th></th><td><input type="submit" value="send"></td></tr>
         </form>
