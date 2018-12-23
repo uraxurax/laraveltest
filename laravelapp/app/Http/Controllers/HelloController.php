47eb597c69ca3d165d93115ff99344a9b33f8463
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-23 23:02:28 +0900

    list 4-26

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -27,15 +27,25 @@ class HelloController extends Controller
         $rules = [
             'name' => 'required',
             'mail' => 'email',
-            'age' => 'numeric|between:0,150',
+            'age' => 'numeric',
         ];
         $messages = [
             'name.required' => '名前は必ず入力して下さい。',
             'mail.email' => 'メールアドレスが必要です。',
             'age.numeric' => '年齢を整数で記入ください。',
-            'age.between' => '年齢は0 - 140の間で入力ください。',
+            'age.min' => '年齢は0才以上で記入ください。',
+            'age.max' => '年齢は200才以下で記入ください。',
         ];
         $validator = Validator::make($request->all(), $rules, $messages);
+        
+        $validator->sometimes('age', 'min:0', function($input){
+            return !is_int($input->age);
+        });
+
+        $validator->sometimes('age', 'max:200', function($input){
+            return !is_int($input->age);
+        });
+    
         if ($validator->fails()) {
             return redirect('/hello')
                         ->withErrors($validator)
