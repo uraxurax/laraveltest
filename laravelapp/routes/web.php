e1d4852cf16f52f88cb37fe127eb8a224347e516
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-23 19:36:32 +0900

    list 4-16

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -13,5 +13,5 @@
 
 // use App\Http\Middleware\HelloMiddleware;
 
-Route::get('hello', 'HelloController@index')->middleware('helo');
+Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
