f7e5cbdc7616d306f710812b2106c09b920f4921
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-24 13:28:10 +0900

    list 5-10

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -15,3 +15,6 @@
 
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
+Route::get('hello/add', 'HelloController@add');
+Route::post('hello/add', 'HelloController@create');
+
