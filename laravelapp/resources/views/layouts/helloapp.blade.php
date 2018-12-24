409ef7f4dbab7becd4c97248a68df4bdaeed677c
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-24 12:14:08 +0900

    list 5-6

--- laravelapp/resources/views/layouts/helloapp.blade.php
+++ laravelapp/resources/views/layouts/helloapp.blade.php
@@ -7,6 +7,8 @@
         margin:-20px 0px -30px 0px; letter-spacing: -4pt; }
     ul { font-size:12pt; }
     hr { margin: 25px 100px; border-top: 1px dashed #ddd; }
+    th {background-color:#999, color:fff; padding:5px 10px; }
+    td {border: solid 1px #aaa; color:#999; padding:5px 10px; }
     .menutitle {font-size:14pt; font-weight:bold; margin: 0px; }
     .content {margin:10px; }
     .foobar { text-align:right; font-size:10pt; margin:10px;
