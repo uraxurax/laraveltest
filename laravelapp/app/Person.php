cb06f2f820433f0b557e8054bcfc5cddb93fc9e1
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-26 15:43:41 +0900

    list 6-17

--- laravelapp/app/Person.php
+++ laravelapp/app/Person.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
-
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
@@ -31,8 +31,6 @@ class Person extends Model
     {
         parent::boot();
 
-        static::addGlobalScope('age', function (Builder $builder) {
-            $builder->where('age', '>', 20);
-        });
+        static::addGlobalScope(new ScopePerson);
     }
 }
