f98d62d931a0838aa94ee5767eba6ca6dc1aa60a
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-26 15:23:35 +0900

    list 6-12

--- laravelapp/app/Person.php
+++ laravelapp/app/Person.php
@@ -10,5 +10,10 @@ class Person extends Model
     {
         return $this->id . ': ' . $this->name . '(' . $this->age . ')';
     }
+
+    public function scopeNameEqual($query, $str)
+    {
+        return $query->where('name', $str);
+    }
     //
 }
