a3978588d5e9ef539ada892c091655d95af713ca
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-26 15:36:46 +0900

    list 6-15

--- laravelapp/app/Person.php
+++ laravelapp/app/Person.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class Person extends Model
 {
@@ -25,5 +27,12 @@ class Person extends Model
     {
         return $query->where('age', '<=', $n);
     }
-    //
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('age', function (Builder $builder) {
+            $builder->where('age', '>', 20);
+        });
+    }
 }
