e8b6ac7d1a1d866bdde62e79aeba03d73d44efe4
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-26 14:50:37 +0900

    list 6-6

--- laravelapp/app/Person.php
+++ laravelapp/app/Person.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
+    public function getData()
+    {
+        return $this->id . ': ' . $this->name . '(' . $this->age . ')';
+    }
     //
 }
