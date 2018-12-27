22aa935a17349ca3e6d2da87f8d026268969604c
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-27 16:50:00 +0900

    list 6-21

--- laravelapp/app/Person.php
+++ laravelapp/app/Person.php
@@ -8,29 +8,11 @@ use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
-    public function getData()
-    {
-        return $this->id . ': ' . $this->name . '(' . $this->age . ')';
-    }
+    protected $guarded = array('id');
 
-    public function scopeNameEqual($query, $str)
-    {
-        return $query->where('name', $str);
-    }
-
-    public function scopeAgeGreaterThan($query, $n)
-    {
-        return $query->where('age', '>=', $n);
-    }
-
-    public function scopeAgeLessThan($query, $n)
-    {
-        return $query->where('age', '<=', $n);
-    }
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::addGlobalScope(new ScopePerson);
-    }
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
 }
