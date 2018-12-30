5dee9445e8b4a87b45dc474d7c881c575c9158da
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-30 19:00:00 +0900

    list 7-15

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -25,6 +25,9 @@ Route::get('hello/show', 'HelloController@show');
 
 Route::get('hello/rest', 'HelloController@rest');
 
+Route::get('hello/session', 'HelloController@ses_get');
+Route::post('hello/session', 'HelloController@ses_put');
+
 Route::get('person', 'PersonController@index');
 Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
