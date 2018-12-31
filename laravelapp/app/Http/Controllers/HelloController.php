b2a99e17c04b97f216194f0ea594e912a9c902d9
uraxurax <urax@wd6.so-net.ne.jp>
2018-12-31 16:08:13 +0900

    list 7-24

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -14,7 +14,7 @@ class HelloController extends Controller
     public function index(Request $request) 
     {
         $sort = $request->sort;
-        $items = Person::orderBy($sort, 'asc')->simplePaginate(5);
+        $items = Person::orderBy($sort, 'asc')->paginate(5);
         $param = ['items' => $items, 'sort' => $sort];
         return view('hello.index', $param);
     }
